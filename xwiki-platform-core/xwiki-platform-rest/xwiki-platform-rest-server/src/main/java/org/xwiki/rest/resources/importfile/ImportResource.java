a32e40c8f3adc67dcd8caf1fd343bf5715782221
tmortagne <thomas.mortagne@gmail.com>
2011-10-24 10:00:23 +0200

    XWIKI-7076: Add REST API to import a xar file

---
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/resources/importfile/ImportResource.java
@@ -0,0 +1,4 @@
+package org.xwiki.rest.resources.importfile;
+Unstaged changes after reset:
+M	xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/resources/importfile/ImportResource.java
+M	xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/resources/importfile/ImportResource2.java
